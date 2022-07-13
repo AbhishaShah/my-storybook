c0439c8c8c055459496d096cd43245254ba34f61
ashah <ashah@opusonesolutions.com>
2022-07-13 11:58:45 -0400

    Added template and passed args to make component dynamic

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -2,9 +2,22 @@ import Button from "../components/Button";
 
 export default {
     title:"Button",
-    component: Button
+    component: Button,
+    argTypes:{ handleClick : {action: "handleClick"} }, // only neede by storyybook
 }
 
-export const Primary = () => <Button label="I'm Primary Button" backgroundColor="blue"/>
+const Template = args => <Button {...args} />
 
-export const Danger = () => <Button label="I'm Danger Button"  backgroundColor="red" size="lg"/>
+export const Primary = Template.bind({}) 
+Primary.args = { // here arg is the props required for our component
+    label:"I'm Primary Button", 
+    backgroundColor:"blue",
+    size:"sm"
+}
+
+export const Danger = Template.bind({}) 
+Danger.args = {
+    label:"I'm Danger Button", 
+    backgroundColor:"red",
+    size:"lg"
+}
